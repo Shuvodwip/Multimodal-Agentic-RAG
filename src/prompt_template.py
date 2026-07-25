cb6fdb6394c309925cfe655554870f08394560f8
Shuvodwip <shuvodwip787@gmail.com>
2026-07-26 01:58:18 +0600

    Add chunk-level citations to prompt template

--- src/prompt_template.py
+++ src/prompt_template.py
@@ -1,4 +1,4 @@
-PROMPT_TEMPLATE = """Answer the question using only the context below. If the context doesn't contain the answer, say you don't know.
+PROMPT_TEMPLATE = """Answer the question using only the context below. Cite the chunk number(s) you relied on in square brackets after each claim, like [Chunk 3]. If the context doesn't contain the answer, say you don't know.
 
 Context:
 {context}
@@ -10,7 +10,10 @@ Answer:"""
 
 def build_prompt(question: str, results: dict) -> str:
     chunks = results["documents"][0]
-    context = "\n\n".join(chunks)
+    metadatas = results["metadatas"][0]
+    context = "\n\n".join(
+        f"[Chunk {meta['chunk_index']}]\n{doc}" for doc, meta in zip(chunks, metadatas)
+    )
     return PROMPT_TEMPLATE.format(context=context, question=question)
 
 
